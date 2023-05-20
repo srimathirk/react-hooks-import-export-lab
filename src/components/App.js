1d6987c877e633b19d842764c402c7e58f31eeee
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-05-19 20:50:46 -0400

    update lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import About from "/home/srimathi/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js";
+import About from "./About.js";
 
 
 function App() {
